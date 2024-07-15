2872ca0ea4d643fb0360dd94b83d20fdd426f607
tonyzzp <tonyzzp@qq.com>
2024-07-15 14:28:17 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.docker.izzp.me": dockerHub,
+  "quay.docker.izzp.me": "https://quay.io",
+  "gcr.docker.izzp.me": "https://gcr.io",
+  "k8s-gcr.docker.izzp.me": "https://k8s.gcr.io",
+  "k8s.docker.izzp.me": "https://registry.k8s.io",
+  "ghcr.docker.izzp.me": "https://ghcr.io",
+  "cloudsmith.docker.izzp.me": "https://docker.cloudsmith.io",
+  "ecr.docker.izzp.me": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.docker.izzp.me": dockerHub,
 };
 
 function routeByHosts(host) {
